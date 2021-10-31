106bc9a8fb3944d18e9335a989aa37728e5a77c0
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-10-31 17:25:43 +0100

    test

--- tests/test_01.py
+++ tests/test_01.py
@@ -1,2 +1,13 @@
-def test_01(fs):
+from pathlib import Path
+import pytest
+
+@pytest.fixture
+def fake_fs_01(fs):
+    path = '/toto'
+    p = Path(path)
+    p.mkdir(parents=True)
+    yield path
+    p.rmdir()
+
+def test_01(fake_fs_01):
     pass
