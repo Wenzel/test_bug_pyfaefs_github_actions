30ab91a2fbe84a09918a05c9a4b2a451a6721724
Mathieu Tarral <mathieu.tarral@protonmail.com>
2021-11-01 09:31:39 +0100

    add libcloud

--- tests/test_01.py
+++ tests/test_01.py
@@ -1,13 +1,25 @@
 from pathlib import Path
 import pytest
+from libcloud.storage.providers import get_driver
+from tempfile import TemporaryDirectory
+
 
 @pytest.fixture
-def fake_fs_01(fs):
-    path = '/toto'
-    p = Path(path)
-    p.mkdir(parents=True)
-    yield path
-    p.rmdir()
+def tmp_directory(fs):
+    with TemporaryDirectory() as tmp_dir:
+        yield tmp_dir
 
-def test_01(fake_fs_01):
+@pytest.fixture
+def fake_libcloud_container(fs, tmp_directory):
+    path = tmp_directory
+    cls = get_driver('local')
+    driver = cls(key=path)
+    container_name = 'cont_01'
+    cont = driver.create_container(container_name)
+    yield driver, cont
+    for cont in driver.iterate_containers():
+        driver.delete_container(cont)
+
+
+def test_01(fake_libcloud_container):
     pass
